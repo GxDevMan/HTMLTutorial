17fb0f9584984c4f9e99243a2826b26a46de854c
Feivel Doctora <82009946+GxDevMan@users.noreply.github.com>
2021-04-08 03:08:28 +0800

    - Java Script Testing

---
+++ public_html/jsfolder/jstest.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function myFunc()
+{
+    document.getElementById("demo").innerHTML = "Changed. ";
+}
+var x = 3;
+var y = 5;
+var z = 4;
+var p = x + y + z;
+
+function exec()
+{
+    document.getElementById("theory").innerHTML = "Hahahahah"
+}
---
+++ public_html/jsfolder/object test.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
