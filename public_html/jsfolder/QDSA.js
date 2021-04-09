2e299df72d25d8896bedbca11fc27df888c8f282
Feivel Doctora <82009946+GxDevMan@users.noreply.github.com>
2021-04-09 15:35:56 +0800

    - Corrected Queue class version

--- public_html/jsfolder/QDSA.js
+++ public_html/jsfolder/QDSA.js
@@ -3,104 +3,91 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class Queue
-{
-    constructor()
-    {
+class Queue {
+    constructor() {
         this.size = 100;
         this.firstpointer = -1;
         this.lastpointer = -1;
-        this.x[size];
+        this.x = Array(this.size);
     }
-    setsize(x)
-    {
-        if (x > 0)
-        {
+    setsize(x) {
+        if (x > 0) {
             this.size = x;
-            this.x = new Array[x];
+            this.x = new Array(x);
         }
     }
-    clear()
-    {
+    clear() {
         this.firstpointer = -1;
         this.lastpointer = -1;
     }
-    isEmpty()
-    {
-        return(this.firstpointer === -1 && this.lastpointer === -1);
+    isEmpty() {
+        return (this.firstpointer === -1 && this.lastpointer === -1);
     }
-    isFull()
-    {
-        return ((this.lastpointer+1) % this.size === this.firstpointer);
+    isFull() {
+        return ((this.lastpointer + 1) % this.size === this.firstpointer);
     }
-    enqueue(any)
-    {
-        if(this.isFull()){return -999;}
-        if(this.isEmpty()){this.firstpointer = 0;}
-        this.lastpointer = (this.lastpointer+1) % this.size;
+    enqueue(any) {
+        if (this.isFull()) {
+            return -999;
+        }
+        if (this.isEmpty()) {
+            this.firstpointer = 0;
+        }
+        this.lastpointer = (this.lastpointer + 1) % this.size;
         this.x[this.lastpointer] = any;
     }
-    dequeue()
-    {
-        if(this.isEmpty()){return null;}
+    dequeue() {
+        if (this.isEmpty()) {
+            return null;
+        }
         var store = this.x[this.lastpointer];
-        if(this.firstpointer === this.lastpointer){this.clear();}
-        else{this.firstpointer = (this.firstpointer+1)%this.size;}
+        if (this.firstpointer === this.lastpointer) {
+            this.clear();
+        } else {
+            this.firstpointer = (this.firstpointer + 1) % this.size;
+        }
         return store;
     }
 }
 var qtest = {
-  size: 100,
-  firstpointer: -1,
-  lastpointer : -1,
-  x: [],
-  isEmpty : function() 
-  {
-      var compare1 = this.firstpointer;
-      var compare2 = this.lastpointer;
-      return (compare1 === -1 && compare2 === -1);
-  },
-  setsize: function(x)
-  {
-      if(x > 0)
-      {
-          this.size = x;
-          this.x = new Array(x);
-      }
-  },
-  clear: function()
-  {
-      this.firstpointer = -1;
-      this.lastpointer = -1;
-  },
-  isFull: function()
-  {
-      return ((this.lastpointer+1) % this.size === this.firstpointer);
-  },
-  enqueue: function(anything)
-  {
-      if(this.isFull()){return -999;}
-      if(this.isEmpty()){this.firstpointer = 0;}
-      this.lastpointer = (this.lastpointer+1) % this.size;
-      this.x[this.lastpointer] = anything;
-  },
-  dequeue: function()
-  {
-      if(this.isEmpty()){return null;}
-      var store = this.x[this.lastpointer];
-      if(this.firstpointer === this.lastpointer){this.clear();}
-      else{this.firstpointer = (this.firstpointer+1)%this.size;}
-      return store;
-  },
-  printall: function()
-  {
-        for (var i = 0; i < this.size; i++)
-        {
+    size: 100,
+    firstpointer: -1,
+    lastpointer: -1,
+    x: [],
+    isEmpty: function() {
+        var compare1 = this.firstpointer;
+        var compare2 = this.lastpointer;
+        return (compare1 === -1 && compare2 === -1);
+    },
+    setsize: function(x) {
+        if (x > 0) {
+            this.size = x;
+            this.x = new Array(x);
+        }
+    },
+    clear: function() {
+        this.firstpointer = -1;
+        this.lastpointer = -1;
+    },
+    isFull: function() {
+        return ((this.lastpointer + 1) % this.size === this.firstpointer);
+    },
+    enqueue: function(anything) {
+        if (this.isFull()) { return -999; }
+        if (this.isEmpty()) { this.firstpointer = 0; }
+        this.lastpointer = (this.lastpointer + 1) % this.size;
+        this.x[this.lastpointer] = anything;
+    },
+    dequeue: function() {
+        if (this.isEmpty()) { return null; }
+        var store = this.x[this.lastpointer];
+        if (this.firstpointer === this.lastpointer) { this.clear(); } else { this.firstpointer = (this.firstpointer + 1) % this.size; }
+        return store;
+    },
+    printall: function() {
+        for (var i = 0; i < this.size; i++) {
             var compile = this.x[i] + " ";
         }
         return compile;
-  }
+    }
 };
-
-
-
--- public_html/jsfolder/qclass.js
+++ public_html/jsfolder/qclass.js
@@ -3,50 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class Queue
-{
-    constructor()
-    {
+class Queue {
+    constructor() {
         this.size = 100;
         this.firstpointer = -1;
         this.lastpointer = -1;
-        this.x(size);
+        this.x = Array(this.size);
     }
-    setsize(x)
-    {
-        if (x > 0)
-        {
+    setsize(x) {
+        if (x > 0) {
             this.size = x;
-            this.x = new Array[x];
+            this.x = new Array(this.x);
         }
     }
-    clear()
-    {
+    clear() {
         this.firstpointer = -1;
         this.lastpointer = -1;
     }
-    isEmpty()
-    {
-        return(this.firstpointer === -1 && this.lastpointer === -1);
+    isEmpty() {
+        return (this.firstpointer === -1 && this.lastpointer === -1);
     }
-    isFull()
-    {
-        return ((this.lastpointer+1) % this.size === this.firstpointer);
+    isFull() {
+        return ((this.lastpointer + 1) % this.size === this.firstpointer);
     }
-    enqueue(any)
-    {
-        if(this.isFull()){return -999;}
-        if(this.isEmpty()){this.firstpointer = 0;}
-        this.lastpointer = (this.lastpointer+1) % this.size;
+    enqueue(any) {
+        if (this.isFull()) {
+            return -999;
+        }
+        if (this.isEmpty()) {
+            this.firstpointer = 0;
+        }
+        this.lastpointer = (this.lastpointer + 1) % this.size;
         this.x[this.lastpointer] = any;
     }
-    dequeue()
-    {
-        if(this.isEmpty()){return null;}
+    dequeue() {
+        if (this.isEmpty()) {
+            return null;
+        }
         var store = this.x[this.lastpointer];
-        if(this.firstpointer === this.lastpointer){this.clear();}
-        else{this.firstpointer = (this.firstpointer+1)%this.size;}
+        if (this.firstpointer === this.lastpointer) {
+            this.clear();
+        } else {
+            this.firstpointer = (this.firstpointer + 1) % this.size;
+        }
         return store;
     }
 }
-let Queue1 = new Queue();
